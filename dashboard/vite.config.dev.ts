575a0ad24b5b4ae2d2d610085d8635d87b5af96b
Savita <sakumari@zynga.com>
2023-06-27 18:43:48 +0530

    build:prod -> build

--- dashboard/vite.config.dev.ts
+++ dashboard/vite.config.dev.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import federation from '@originjs/vite-plugin-federation';
+import packageJson from './package.json'
+
 
 export default defineConfig({
   plugins: [
